96ea8489e445a754a323ceec80460389a033e9d7
Tom Arnoldussen <tom.arnoldussen@gmail.com>
2014-06-17 12:19:32 +0200

    Bakkes

--- geo/www/js/googlemaps.js
+++ geo/www/js/googlemaps.js
@@ -7,11 +7,9 @@
     dataType: "json",
     success: function(data){  
       if(!(jQuery.isEmptyObject(data))) {
-   
-   callback(data);       
+      callback(data);       
+      console.log("data"+data.location[1].location_lat);
       }
-       
-
 
     }
     ,
@@ -21,55 +19,61 @@
   });
 
 }
- 
-function GoogleMap(lat, lng, canvas, myPoints) {
- 	var canvas = canvas;
-	var id = "1";
-	var markers = new Array();
-	var baseUrl = 'http://pienvandalen.nl/';
- 	this.initialize = function(){
-		var map = showMap();
-	}
- 	var showMap = function(){
-        var myOptions = {
-            center: new google.maps.LatLng(lat,lng),
-            zoom: 15,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
+function loadMap(data) {
+    $(function(){
+    getGeolocation();
+    });
+    var isDeviceReady = false;
+    document.addEventListener("deviceready", onDeviceReady, false);
+    function onDeviceReady() 
+    {
+    isDeviceReady = true;
+    }
+    function getGeolocation()
+    {
+    navigator.geolocation.getCurrentPosition(onGetGeolocationSuccess, onGetGeolocationError);
+    }
+    function onGetGeolocationSuccess(position)
+    {
+    /*html = "Latitude: " + position.coords.latitude + "<br />" +
+    "Longitude: " + position.coords.longitude + "<br />" +
+    "Altitude: " + position.coords.altitude + "<br />" + 
+    "Accuracy: " + position.coords.accuracy + "<br />" + 
+    "Altitude Accuracy: " + position.coords.altitudeAccuracy + "<br />" + 
+    "Heading: " + position.coords.heading + "<br />" + 
+    "Speed: " + position.coords.speed + "<br />" + 
+    "Timestamp: " + new Date(position.timestamp) + "<br />"; 
+    $("#dGeolocation").html(html);
+    */
 
-        var map = new google.maps.Map(canvas, myOptions);
+   
+    var map = new google.maps.Map(document.getElementById('map_canvas'), {
+    zoom: 12,
+    center: new google.maps.LatLng(position.coords.latitude , position.coords.longitude),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
 
-        setMarkers(map, myPoints);
-        
-
-
-        
-        
-        
-        function setMarkers(map, markers) {
-
-
-
-        for (var i = 0; i < markers.location.length; i++) {
-        	console.log("aantal"+ i);
-            var sites = markers.location[i];
-   			
-            var siteLatLng = new google.maps.LatLng(sites.location_lat,sites.location_lon);
-            var marker = new google.maps.Marker({
-                position: siteLatLng,
-                map: map
-            });
-            
-			
-
-
-            
-        }
-
-return marker;
-        // Set all markers in the myPoints variable
-          
+    var infowindow = new google.maps.InfoWindow();
+    var marker, i;
+    var image= "asset/img/images.jpg";
+   
+    for (i = 0; i < data.location.length; i++) { 
+    marker = new google.maps.Marker({
+    position: new google.maps.LatLng(data.location[i].location_lat, data.location[i].location_lon),
+    map: map
+    });
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+    return function() {
+    infowindow.setContent(data.location[i].date);
+    infowindow.open(map, marker);
+    }
+    })(marker, i));
+    }
+    }
+    function onGetGeolocationError(error)
+    {
+    html = "Error code: " + error.code + "<br />" + 
+    "Error message: " + error.message + "<br />";
+    $("#dGeolocation").html(html);
     }
 }
-}
-
